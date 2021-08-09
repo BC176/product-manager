599cb18d272ab41c467814dc61979c023644db4f
Brian Clark <brian.w.clark176@gmail.com>
2021-08-09 15:37:04 -0600

    updated to display a list of newly added items on page refresh

--- Main.js
+++ Main.js
@@ -1,12 +1,41 @@
 // import React, { useEffect, useState } from 'react';
 // import axios from 'axios';
-import ProductForm from '../components/ProductForm';
+// import ProductForm from '../components/ProductForm';
+// import ProductList from '../components/ProductList';
 
+// const Main = () => {
+//     return (
+//         <div>
+//             <ProductForm />
+//         </div>
+//     )
+// }
+// export default Main;
+
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import ProductList from '../components/ProductList';
+import ProductForm from '../components/ProductForm';
 const Main = () => {
+    const [product, setProduct] = useState([]);
+    const [loaded, setLoaded] = useState(false);
+    useEffect(() => {
+        axios.get('http://localhost:8000/api/product')
+            .then(res => {
+                console.log(res.data);
+                setProduct(res.data.products);
+                setLoaded(true);
+
+            });
+    }, [])
     return (
         <div>
             <ProductForm />
+            <hr />
+            {loaded && <ProductList product={product} />}
         </div>
     )
 }
 export default Main;
+
+
